69a0d6a2aa006e3611fc7c113438c0e826313915
Vincent Rischmann <vincent@rischmann.fr>
2015-06-13 18:34:10 +0200

    Inital commit

---
+++ logr.go
@@ -0,0 +1,146 @@
+package logr
+
+import (
+	"fmt"
+	"os"
+	"sync"
+	"time"
+)
+
+const (
+	SuffixTimeFormat = "2006-01-02_1504"
+)
+
+// RotatingWriter is a io.Writer which wraps a *os.File, suitable for log rotation.
+type RotatingWriter struct {
+	lock     sync.Mutex
+	filename string
+	file     *os.File
+
+	schedule struct {
+		hour   int
+		minute int
+	}
+
+	currentSize int64
+	maxSize     int64
+}
+
+// NewWriter creates a new file and returns a rotating writer.
+func NewWriter(filename string) (*RotatingWriter, error) {
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_APPEND, 0600)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewWriterFromFile(file)
+}
+
+func NewWriterFromFile(file *os.File) (*RotatingWriter, error) {
+	w := &RotatingWriter{
+		filename: file.Name(),
+		file:     file,
+		maxSize:  -1,
+	}
+
+	w.schedule.hour = -1
+	w.schedule.minute = -1
+
+	if err := w.readCurrentSize(); err != nil {
+		return nil, err
+	}
+
+	return w, nil
+}
+
+// readCurrentSize reads the current size from the file
+func (w *RotatingWriter) readCurrentSize() error {
+	fi, err := w.file.Stat()
+	if err != nil {
+		return err
+	}
+
+	w.currentSize = fi.Size()
+
+	return nil
+}
+
+// Schedule set the time at which to rotate, each day
+func (w *RotatingWriter) Schedule(hour, minute int) *RotatingWriter {
+	w.lock.Lock()
+	defer w.lock.Unlock()
+
+	w.schedule.hour = hour
+	w.schedule.minute = minute
+
+	return w
+}
+
+// MaxSize set the size at which to rotate the file
+func (w *RotatingWriter) MaxSize(s int64) *RotatingWriter {
+	w.lock.Lock()
+	defer w.lock.Unlock()
+
+	w.maxSize = s
+
+	return w
+}
+
+func (w *RotatingWriter) Write(b []byte) (int, error) {
+	w.lock.Lock()
+	defer w.lock.Unlock()
+
+	if w.schedule.hour > -1 {
+		now := time.Now()
+		if now.Hour() == w.schedule.hour {
+			if err := w.rotate(); err != nil {
+				return -1, err
+			}
+		}
+	}
+
+	if w.maxSize > -1 {
+		if w.currentSize >= w.maxSize {
+			if err := w.rotate(); err != nil {
+				return -1, err
+			}
+		}
+	}
+
+	n, err := w.file.Write(b)
+	w.currentSize += int64(n)
+
+	return n, err
+}
+
+// rotate rotates the file. must be called while having the file lock
+func (w *RotatingWriter) rotate() error {
+	if err := w.file.Close(); err != nil {
+		return err
+	}
+
+	{
+		destName := w.filename + "." + time.Now().Format(SuffixTimeFormat)
+		_, err := os.Stat(destName)
+		if err != nil && !os.IsNotExist(err) {
+			fmt.Println(err.Error())
+			return err
+		}
+
+		if err := os.Rename(w.filename, destName); err != nil {
+			return err
+		}
+	}
+
+	{
+		file, err := os.OpenFile(w.filename, os.O_RDWR|os.O_CREATE, 0600)
+		if err != nil {
+			return err
+		}
+
+		w.file = file
+		w.currentSize = 0
+	}
+
+	return nil
+}
---
+++ logr_test.go
@@ -0,0 +1,92 @@
+package logr_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/vrischmann/logr"
+)
+
+func makeBuf(b byte) []byte {
+	buf := make([]byte, 1024)
+	for i := 0; i < len(buf); i++ {
+		buf[i] = b
+	}
+
+	return buf
+}
+
+func readFile(t testing.TB, filename string) []byte {
+	data, err := ioutil.ReadFile(filename)
+	require.Nil(t, err)
+
+	return data
+}
+
+func checkEqual(t testing.TB, buf []byte, b byte) error {
+	for _, v := range buf {
+		if v != b {
+			return fmt.Errorf("%v != %v", v, b)
+		}
+	}
+
+	return nil
+}
+
+func TestRotateSchedule(t *testing.T) {
+	f, err := ioutil.TempFile(os.TempDir(), "logr")
+	require.Nil(t, err)
+
+	rw, err := logr.NewWriterFromFile(f)
+	require.Nil(t, err)
+
+	now := time.Now()
+	{
+		n, err := rw.Write(makeBuf(0xFF))
+		require.Nil(t, err)
+		require.Equal(t, 1024, n)
+
+		rw.Schedule(now.Hour(), now.Minute())
+
+		n, err = rw.Write(makeBuf(0xFE))
+		require.Nil(t, err)
+		require.Equal(t, 1024, n)
+	}
+
+	newData := readFile(t, f.Name())
+	require.Nil(t, checkEqual(t, newData, 0xFE))
+
+	rotatedData := readFile(t, f.Name()+"."+now.Format(logr.SuffixTimeFormat))
+	require.Nil(t, checkEqual(t, rotatedData, 0xFF))
+}
+
+func TestRotateMaxSize(t *testing.T) {
+	f, err := ioutil.TempFile(os.TempDir(), "logr")
+	require.Nil(t, err)
+
+	rw, err := logr.NewWriterFromFile(f)
+	require.Nil(t, err)
+
+	now := time.Now()
+	{
+		n, err := rw.Write(makeBuf(0xFF))
+		require.Nil(t, err)
+		require.Equal(t, 1024, n)
+
+		rw.MaxSize(512)
+
+		n, err = rw.Write(makeBuf(0xFE))
+		require.Nil(t, err)
+		require.Equal(t, 1024, n)
+	}
+
+	newData := readFile(t, f.Name())
+	require.Nil(t, checkEqual(t, newData, 0xFE))
+
+	rotatedData := readFile(t, f.Name()+"."+now.Format(logr.SuffixTimeFormat))
+	require.Nil(t, checkEqual(t, rotatedData, 0xFF))
+}
